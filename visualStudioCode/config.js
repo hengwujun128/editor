dce055efede3e6eb5b540add2d7753bfd0506c5c
hengwujun <hengwujun128@gmail.com>
2018-04-15 21:15:37 +0800

    add visual studio code

---
+++ visualStudioCode/config.js
@@ -0,0 +1,25 @@
+{
+    //-------- 编辑器配置 --------
+    "editor.fontSize": 14,
+    // 控制行高。
+    "editor.lineHeight": 0,
+    "workbench.iconTheme": "material-icon-theme",
+    "editor.fontFamily": "Consolas, 'Courier New', monospace",
+    // "editor.fontFamily": "Consolas, 'Courier New', monospace",
+    "workbench.startupEditor": "newUntitledFile",
+    "material-icon-theme.showUpdateMessage": false,
+    "html.format.preserveNewLines": false,
+    // 标记列表，以逗号分隔，其前应有额外新行。"null" 默认为“标头、正文、/html”。
+    "html.format.extraLiners": "",
+    "editor.colorDecorators": true,
+    "explorer.confirmDelete": false,
+    "editor.formatOnSave": true,
+    // 按 "Tab" 时插入空格。
+    "editor.insertSpaces": true,
+    // 一个制表符等于的空格数。
+    "editor.tabSize": 2,
+    // 控制光标样式，接受的值为 'block' 和 'line'
+    "editor.cursorStyle": "block",
+    // 删除尾随自动插入的空格
+    "editor.trimAutoWhitespace": true,
+}
