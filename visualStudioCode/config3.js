9e2392d2e8721f71da5b07c98ebbe5ac7c62e9b1
zwx544198 <zhangzequan2@huawei.com>
2019-01-07 17:53:45 +0800

    modify sth

--- visualStudioCode/config3.js
+++ visualStudioCode/config3.js
@@ -1,14 +1,10 @@
-// 团队云使用的配置
 {
-  //-------- 编辑器配置 --------
   "editor.fontSize": 16,
   // 控制行高。
   "editor.lineHeight": 0,
-  "workbench.iconTheme": "vscode-icons",
   // "editor.fontFamily": "Consolas, 'Courier New', monospace",
   "editor.fontFamily": "'Courier New', monospace",
   "workbench.startupEditor": "newUntitledFile",
-  "material-icon-theme.showUpdateMessage": false,
   "html.format.preserveNewLines": false,
   // 标记列表，以逗号分隔，其前应有额外新行。"null" 默认为“标头、正文、/html”。
   "html.format.extraLiners": "",
@@ -36,22 +32,29 @@
   "editor.wordWrap": "on",
   // 覆盖当前所选颜色主题的颜色。
   "workbench.colorCustomizations": {},
-  "editorCursor.foreground": "#ffffff",
   "git.autofetch": true,
   // 启用/禁用默认 HTML 格式化程序
   "html.format.enable": true,
   "gitlens.historyExplorer.enabled": true,
-  "vsicons.projectDetection.autoReload": true,
+  /**
+    We can still optimize ESLint by configuring it
+    to perform code formatting every time we save the file
+    */
 
-
-
-  // eslint 配置
+  //
   "eslint.options": {
-    "configFile": "E:/project/smartODN/WebPortal-VUE/.eslintrc.js"
-    // "configFile": "E:/project/github-projects/vue-test/.eslintrc.js"
+    // 要用项目中的eslint配置文件，覆盖用户设置的全局的配置文件
+    "configFile": "D:/project/smartODN/web/WebPortal-VUE/.eslintrc.js",
+    "plugins": ["html"]
   },
   "eslint.autoFixOnSave": true,
-  "eslint.validate": [{
+  // VSCode will perform validation for these three file types: vue, HTML and JavaScript.
+  "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    "html",
+    "vue",
+    {
       "language": "vue",
       "autoFix": true
     },
@@ -66,10 +69,28 @@
   ],
 
 
-  // vetur 配置，主要是vue模板要使用此配置
-  "vetur.format.defaultFormatter.js": "vscode-typescript",
-  "vetur.format.defaultFormatter.html": "js-beautify-html",
+
+  // 参考vetur 官网配置(https://vuejs.github.io/vetur/formatting.html#settings)
+  "vetur.format.defaultFormatterOptions": {
+    // Prettier option here
+    "prettier": {
+      "semi": false, // Whether to add a semicolon at the end of every line
+      "singleQuote": true, // If true, will use single instead of double quotes
+      "eslintIntegration": true, //eslint 集成
+
+    }
+  },
+
+  //html格式化依赖  默认为none,js-beautifuy-html
+  "vetur.format.defaultFormatter.html": "prettyhtml",
+  // js 格式化
+  // "vetur.format.defaultFormatter.js": "vscode-typescript",
+  "vetur.format.defaultFormatter.js": "prettier",
+  //函数前加空格
   "javascript.format.insertSpaceBeforeFunctionParenthesis": true,
 
-  "vsicons.dontShowNewVersionMessage": true
+
+  "terminal.integrated.rendererType": "dom",
+  "gitlens.views.fileHistory.enabled": true,
+  "gitlens.views.lineHistory.enabled": true
 }
